71b7aefc941872ef7d4e512a41754d63e6e42492
Ayo <heyayomideadebisi@gmail.com>
2023-09-15 13:50:28 +0100

    remove mistake typecript

---
+++ frontend-task-2/src/pages/movies/[id].jsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+const MoviePage = () => {
+  const router = useRouter()
+  // console.log(router)
+  return (
+    <div>MoviePage</div>
+  )
+}
+
+export default MoviePage
