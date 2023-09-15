4366f8e1fd335c563051436c41f8ba92f20c3a31
Ayo <heyayomideadebisi@gmail.com>
2023-09-15 18:35:44 +0100

    finish movie body

--- frontend-task-2/src/components/Header.jsx
+++ frontend-task-2/src/components/Header.jsx
@@ -3,11 +3,14 @@ import Logo from "../assets/Logo.svg"
 import menu from "../assets/Menu.svg"
 import Image from 'next/image'
 import { Search} from "lucide-react"
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <div className='pt-4 px-2 sm:px-6 md:px-10 lg:px-20 w-full flex items-center text-white justify-between'>
+        <Link href="/">
         <Image src={Logo} alt='Logo'/>
+        </Link>
         <div className='flex relative'>
             <input type="text" placeholder='What do you want to watch?' className='hidden md:inline border border-slate-200 rounded px-2 py-2 bg-transparent w-96 placeholder:text-white' />
             <Search className='md:absolute right-2 top-2 text-white'/>
--- frontend-task-2/src/components/movie/Body.jsx
+++ frontend-task-2/src/components/movie/Body.jsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Header from "../Header";
 import play from "../../assets/Play.svg";
+import List from "../../assets/List.svg";
+import Ticket from "../../assets/Two Tickets.svg";
+import Footer from "../Footer";
+import Link from "next/link";
 
 const Body = ({ data }) => {
-  console.log(data);
   const bannerPath = "https://image.tmdb.org/t/p/original";
   const url = bannerPath + data?.backdrop_path;
+  console.log(data);
 
   return (
     <div>
@@ -20,14 +24,62 @@ const Body = ({ data }) => {
           <Header />
 
           <div className="flex flex-col gap-2 items-center justify-center h-full">
-            <Image src={play} alt="play-icon" className="h-14 w-14 md:h-20 md:w-20" />
-            <p className="text-white font-medium sm:text-lg text-2xl">Watch Trailer</p>
+            <Image
+              src={play}
+              alt="play-icon"
+              className="h-14 w-14 md:h-20 md:w-20"
+            />
+            <p className="text-white font-medium sm:text-lg text-2xl">
+              Watch Trailer
+            </p>
           </div>
-
         </div>
       </div>
 
-      Hello Details
+      <div className="py-4 px-4 sm:py-10 sm:px-10 md:px-20 md:pt-20">
+        {/* top row */}
+        <div className="">
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-4">
+            <p className="text-2xl font-bold">{data?.title} . </p>
+            <p className="text-lg font-medium text-slate-500">
+              Runtime (in minutes): {data?.runtime} .{" "}
+            </p>
+            <p className="text-lg font-medium text-slate-500">
+              Release Date in (UTC): <span>{data?.release_date} . </span>
+            </p>
+          </div>
+        </div>
+
+        {/* overview and two buttons */}
+        <div className="mt-4 flex flex-col md:flex-row justify-between gap-4">
+          <p className="text-sm md:text-base font-medium text-slate-900 md:max-w-[80%]">
+            {data?.overview}
+          </p>
+          <div className="grid sm:grid-cols-2 md:grid-cols-1 gap-4">
+            <button className="bg-red-700 px-4 py-2 text-white font-medium rounded md:w-48 ">
+              <Link
+                href="/"
+                className="flex items-center justify-center gap-2 md:gap-0"
+              >
+                <Image src={Ticket} alt="ticket" className="" />
+                See Showtimes
+              </Link>
+            </button>
+
+            <button className="border border-red-700 bg-red-700/40 px-4 py-2 text-black font-medium rounded md:w-48">
+              <Link
+                href="/"
+                className="flex items-center justify-center gap-2 md:gap-0"
+              >
+                <Image src={List} alt="list" className="" />
+                More watch options
+              </Link>
+            </button>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
     </div>
   );
 };
