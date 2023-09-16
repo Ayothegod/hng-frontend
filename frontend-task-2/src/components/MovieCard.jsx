2175a0e850119e21ed257b4e6d0f5193e048c10f
Ayo <heyayomideadebisi@gmail.com>
2023-09-16 23:19:30 +0100

    daily commit

--- frontend-task-2/src/components/MovieCard.jsx
+++ frontend-task-2/src/components/MovieCard.jsx
@@ -11,6 +11,7 @@ const MovieCard = ({ movie }) => {
   const popularity = Math.floor(movie?.popularity);
 
   const [heart, setHeart] = useState(true);
+  console.log("User");
 
   return (
     <div className="relative" data-testid="movie-card">
