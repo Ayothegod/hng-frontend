99e997f8d9616d51591a6ce001baf43a90212f87
Ayo <heyayomideadebisi@gmail.com>
2023-09-19 20:52:15 +0100

    feat: complete imagedata

--- frontend-task-3/src/lib/imageData.js
+++ frontend-task-3/src/lib/imageData.js
@@ -1,20 +1,143 @@
-import forest1 from "../assets/dave--unsplash.jpg"
-import forest2 from "../assets/forest-2-unsplash.jpg"
-import forest3 from "../assets/forest-3-unsplash.jpg"
-import forest4 from "../assets/forest-unsplash.jpg"
-import mountain1 from "../assets/mountain-unsplash.jpg"
-import mountainForest from "../assets/mountains-forest-unsplash.jpg"
-import mountainIce from "../assets/mountains-ice-unsplash.jpg"
-import mountain4 from "../assets/mountains-unsplash.jpg"
-import mountain5 from "../assets/marek-piwnicki-unsplash.jpg"
-import iceMountain from "../assets/ice-mountain-unsplash.jpg"
-import iceSnow from "../assets/icesnow-unsplash.jpg"
-import space1 from "../assets/spacetype-unsplash.jpg"
-import space2 from "../assets/space-unsplash.jpg"
-import space3 from "../assets/space-2-unsplash.jpg"
-import waterMountain from "../assets/water-mountain-unsplash.jpg"
-import water2 from "../assets/water-unsplash.jpg"
-import water3 from "../assets/watertype-unsplash.jpg"
-import wildlife from "../assets/wildlife-2-unsplash.jpg"
-import wildlife2 from "../assets/wildlife-unsplash.jpg"
-import wildlifeWater from "../assets/wildlife-water-unsplash.jpg"
+import forest1 from "../assets/dave--unsplash.jpg";
+import forest2 from "../assets/forest-2-unsplash.jpg";
+import forest3 from "../assets/forest-3-unsplash.jpg";
+import forest4 from "../assets/forest-unsplash.jpg";
+import mountain1 from "../assets/mountain-unsplash.jpg";
+import mountainForest from "../assets/mountains-forest-unsplash.jpg";
+import mountainIce from "../assets/mountains-ice-unsplash.jpg";
+import mountain4 from "../assets/mountains-unsplash.jpg";
+import mountain5 from "../assets/marek-piwnicki-unsplash.jpg";
+import iceMountain from "../assets/ice-mountain-unsplash.jpg";
+import iceSnow from "../assets/icesnow-unsplash.jpg";
+import space1 from "../assets/spacetype-unsplash.jpg";
+import space2 from "../assets/space-unsplash.jpg";
+import space3 from "../assets/space-2-unsplash.jpg";
+import waterMountain from "../assets/water-mountain-unsplash.jpg";
+import water2 from "../assets/water-unsplash.jpg";
+import water3 from "../assets/watertype-unsplash.jpg";
+import wildlife from "../assets/wildlife-2-unsplash.jpg";
+import wildlife2 from "../assets/wildlife-unsplash.jpg";
+import wildlifeWater from "../assets/wildlife-water-unsplash.jpg";
+
+export const imageData = [
+  {
+    id: 1,
+    imgUrl: forest1,
+    alt: "forest image",
+    tags: ["forest"],
+  },
+  {
+    id: 2,
+    imgUrl: forest2,
+    alt: "forest image",
+    tags: ["forest"],
+  },
+  {
+    id: 3,
+    imgUrl: forest3,
+    alt: "forest image",
+    tags: ["forest"],
+  },
+  {
+    id: 4,
+    imgUrl: forest4,
+    alt: "forest image",
+    tags: ["forest"],
+  },
+  {
+    id: 5,
+    imgUrl: mountain1,
+    alt: "mountain image",
+    tags: ["mountain"],
+  },
+  {
+    id: 6,
+    imgUrl: mountainForest,
+    alt: "mountain image",
+    tags: ["mountain", "forest"],
+  },
+  {
+    id: 7,
+    imgUrl: mountainIce,
+    alt: "mountain image",
+    tags: ["mountain", "ice"],
+  },
+  {
+    id: 8,
+    imgUrl: mountain4,
+    alt: "mountain image",
+    tags: ["mountain"],
+  },
+  {
+    id: 9,
+    imgUrl: mountain5,
+    alt: "mountain image",
+    tags: ["mountain"],
+  },
+  {
+    id: 10,
+    imgUrl: iceMountain,
+    alt: "mountain image",
+    tags: ["ice", "mountain"],
+  },
+  {
+    id: 11,
+    imgUrl: iceSnow,
+    alt: "ice image",
+    tags: ["ice", "snow"],
+  },
+  {
+    id: 12,
+    imgUrl: space1,
+    alt: "space image",
+    tags: ["space"],
+  },
+  {
+    id: 13,
+    imgUrl: space2,
+    alt: "space image",
+    tags: ["space"],
+  },
+  {
+    id: 14,
+    imgUrl: space3,
+    alt: "space image",
+    tags: ["space"],
+  },
+  {
+    id: 15,
+    imgUrl: waterMountain,
+    alt: "water image",
+    tags: ["water", "mountain"],
+  },
+  {
+    id: 16,
+    imgUrl: water2,
+    alt: "water image",
+    tags: ["water"],
+  },
+  {
+    id: 17,
+    imgUrl: water3,
+    alt: "water image",
+    tags: ["water"],
+  },
+  {
+    id: 18,
+    imgUrl: wildlife,
+    alt: "wildlife image",
+    tags: ["wildlife"],
+  },
+  {
+    id: 19,
+    imgUrl: wildlife2,
+    alt: "wildlife image",
+    tags: ["wildlife"],
+  },
+  {
+    id: 20,
+    imgUrl: wildlifeWater,
+    alt: "wildlife image",
+    tags: ["water", "wildlife"],
+  },
+];
