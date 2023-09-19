cfb915951625effba3cb1cbbc1a91223338081ee
Ayo <heyayomideadebisi@gmail.com>
2023-09-19 12:20:30 +0100

    init tailwindcss

--- frontend-task-3/src/pages/_app.js
+++ frontend-task-3/src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>HNGx Image Gallery</title>
+        <meta name="description" content="Built for HNGx frontend task 3" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
