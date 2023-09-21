34c92e2cc7de1a8ed2932459fd0e649648dc7d29
Ayo <heyayomideadebisi@gmail.com>
2023-09-21 02:03:30 +0100

    feat: add supabase client

--- frontend-task-3/src/components/Signin.jsx
+++ frontend-task-3/src/components/Signin.jsx
@@ -1,21 +1,29 @@
+import { supabaseClient } from "@/lib/supabase";
 import { Loader2, Loader2Icon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Signin = () => {
-    const router = useRouter()
-  const { isLoaded, signIn } = useSignIn();
+  const router = useRouter();
+
+  const signin = async () => {
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+      email: "example@email.com",
+      password: "example-password",
+    });
+    console.log(data, error);
+  };
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  
+
   return (
     <div className="flex items-center justify-center h-full">
       <div className="flex flex-col gap-4 px-4">
         <p className="font-semibold text-2xl">Sign-in to access the gallery.</p>
 
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form className="space-y-4" onSubmit={signin}>
           <input
             type="text"
             placeholder="email address..."
@@ -31,7 +39,7 @@ const Signin = () => {
             className="w-full bg-transparent border-2 border-slate-800 rounded px-2 py-2 md:py-2 md:px-4 focus:ring"
           />
 
-          <button className="w-full bg-slate-800 border-none rounded px-2 py-2 md:py-2 md:px-4 text-white font-medium text-lg">
+          <button className="w-full bg-slate-800 border-none rounded px-2 py-2 md:py-2 md:px-4 text-white font-medium text-lg" onClick={signin}>
             get access
           </button>
         </form>
