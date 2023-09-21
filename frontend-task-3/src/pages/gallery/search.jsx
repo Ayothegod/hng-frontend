47968cb746ff6d50ca2c70aa6425aa13b40e82a8
Ayo <heyayomideadebisi@gmail.com>
2023-09-21 19:23:54 +0100

    feat: add text if no search result

--- frontend-task-3/src/pages/gallery/search.jsx
+++ frontend-task-3/src/pages/gallery/search.jsx
@@ -7,7 +7,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import update from "immutability-helper";
 import { TouchBackend } from "react-dnd-touch-backend";
 import ImageBox from "@/components/ImageBox";
-
+import Link from "next/link";
 
 const Search = () => {
   const { searchData, setSearchData } = useSearchContext();
@@ -15,24 +15,25 @@ const Search = () => {
   const [displayData, setDisplayData] = useState([]);
   const [touchDevice, setTouchDevice] = useState(false);
 
+  console.log(displayData);
   useEffect(() => {
-
     const stuff = () => {
       const filteredImages = data.filter((data) =>
-        data.tags.some((tag) => tag.toLowerCase().includes(searchData.toLowerCase()))
+        data.tags.some((tag) =>
+          tag.toLowerCase().includes(searchData.toLowerCase())
+        )
       );
       setDisplayData(filteredImages);
     };
-    stuff()
-
-  },[])
+    stuff();
+  }, []);
 
   useEffect(() => {
     if ("ontouchstart" in window) {
       setTouchDevice(true);
     }
   }, []);
-  
+
   const moveImage = (dragIndex, hoverIndex) => {
     const draggedImage = displayData[dragIndex];
 
@@ -48,11 +49,25 @@ const Search = () => {
   return (
     <main className="min-h-screen bg-gray-100">
       <main className="container max-w-6xl mx-auto px-4 md:px-8 pb-8">
-        <Header/>
-        <div className="my-4">
+        <Header />
+        <div className="my-4 flex items-center gap-4">
+          <Link href="/gallery" className="cursor-pointer">
+            <p className="font-bold text-sm underline text-slate-500 cursor-pointer">
+              go back
+            </p>
+          </Link>
           <p className="text-lg font-medium font-sans">Search Results</p>
         </div>
-        
+
+        {displayData.length < 1 && (
+          <>
+            <p className="mt-10">Your search did not match any image</p>
+            <Link href="/gallery" className="cursor-pointer">
+              <p className="text-sm text-slate-500">Return to gallery</p>
+            </Link>
+          </>
+        )}
+
         {touchDevice && (
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-8">
             {displayData.slice(0, 19).map((data, index) => (
